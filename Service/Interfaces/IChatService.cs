e25f3a12549da864d6fe0b09d801eb8f3897c4e4
felipe-tt <felipeittner5@gmail.com>
2022-08-29 15:38:54 -0300

    commit

--- Service/Interfaces/IChatService.cs
+++ Service/Interfaces/IChatService.cs
@@ -9,5 +9,6 @@ namespace Service.Interfaces
         Task<Response> Update(Mensagem a);
         Task<DataResponse<Mensagem>> GetAll();
         Task<SingleResponse<Mensagem>> GetByID(int id);
+        Task<Response> Excluir(int id);
     }
 }
